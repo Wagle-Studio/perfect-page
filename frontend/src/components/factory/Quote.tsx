3d59565d615c1998020cb3c8744637004dd132a1
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-03 21:30:51 +0200

    Add factory quote style

--- frontend/src/components/factory/Quote.tsx
+++ frontend/src/components/factory/Quote.tsx
@@ -1,8 +1,14 @@
+import "./quote.css";
+
 export type QuoteProps = {
   key: number;
   content: string;
 };
 
 export function Quote(props: QuoteProps) {
-  return <blockquote key={props.key}>{props.content}</blockquote>;
+  return (
+    <blockquote key={props.key} className="quote_container">
+      {props.content}
+    </blockquote>
+  );
 }
