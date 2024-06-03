0ff76c0bf2c49f3f921669eeb3dfef6bca7de70a
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-03 22:07:46 +0200

    Add factory list style

--- frontend/src/components/factory/List.tsx
+++ frontend/src/components/factory/List.tsx
@@ -1,4 +1,5 @@
 import { BlockTags } from "@/types/Block";
+import "./list.css";
 
 export type ListProps = {
   key: number;
@@ -9,5 +10,9 @@ export type ListProps = {
 export function List(props: ListProps) {
   const Tag = props.tag;
 
-  return <Tag key={props.key}>{props.content}</Tag>;
+  return (
+    <Tag key={props.key} className="list_container">
+      {props.content}
+    </Tag>
+  );
 }
