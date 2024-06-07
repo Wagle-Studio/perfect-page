e404d98396813af774e18ff4f2ee46751b7f57c5
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-08 00:37:34 +0200

    Delete the homepage and create a test page per component

---
+++ frontend/src/app/heading_2/page.tsx
@@ -0,0 +1,17 @@
+import { Client } from "@notionhq/client";
+import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { Article } from "@/components/Article";
+
+export default async function Heading2() {
+  const notionClient = new Client({
+    auth: process.env.NOTION_API_KEY,
+  });
+
+  const data = await notionClient.blocks.children.list({
+    block_id: process.env.NOTION_BLOCK_DEV_HEADING_2 ?? "",
+  });
+
+  const blocks = data.results as BlockObjectResponse[];
+
+  return <div>{blocks && <Article blocks={blocks} />}</div>;
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,3 @@
-import { Client } from "@notionhq/client";
-import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
-import { Article } from "@/components/Article";
-
 export default async function Home() {
-  const notionClient = new Client({
-    auth: process.env.NOTION_API_KEY,
-  });
-
-  const data = await notionClient.blocks.children.list({
-    block_id: process.env.NOTION_BLOCK_DEV_ID ?? "",
-  });
-
-  const blocks = data.results as BlockObjectResponse[];
-
-  return <div>{blocks && <Article blocks={blocks} />}</div>;
+  return <div>Accueil</div>;
 }
