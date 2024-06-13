c1e56113f724ad808b4baf26e63f0a0ebeebbdc7
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-13 17:54:48 +0200

    Remove homepage useless scss import

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/ui/web/components/header/Header";
 import { Hero } from "@/ui/web/components/hero/Hero";
-import "./page.scss";
 
 export default async function Home() {
   return (
