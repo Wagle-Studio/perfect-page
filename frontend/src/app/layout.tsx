f5d3424f49abc8dbcbc5d28e08f1c63864a92b07
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-13 17:52:27 +0200

    Move globals styles in the ui part + Update layout meta data

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Portfolio Kévin Wolff",
+  title: "Perfect Page",
+  description: "Perfect Page",
 };
 
 export default function RootLayout({
