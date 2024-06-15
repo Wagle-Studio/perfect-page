822f05687600954b2be68c4ed30087db64d47e9b
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-15 17:11:55 +0200

    Update web icons components

--- frontend/src/ui/web/components/icons/MenuIcon.tsx
+++ frontend/src/ui/web/components/icons/MenuIcon.tsx
@@ -1,15 +1,19 @@
+import classNames from "classnames";
 import { IconProps } from "@/ui/web/components/icons/IconProps";
 import "./icon.scss";
 
-export function MenuIcon(props: IconProps) {
+export function MenuIcon({ size, className, ...props }: IconProps) {
+  const iconClasses = classNames("icon", `icon--${size}`, className);
+
   return (
     <svg
-      className={`icon icon--${props.size}`}
+      className={iconClasses}
       stroke="currentColor"
       strokeWidth="2"
       viewBox="0 0 24 24"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...props}
     >
       <line x1="3" y1="12" x2="21" y2="12"></line>
       <line x1="3" y1="6" x2="21" y2="6"></line>
