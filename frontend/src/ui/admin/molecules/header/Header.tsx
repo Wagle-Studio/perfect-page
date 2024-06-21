ee0806d4717e6d32a4545cdd517511479ad2c1d6
Wolff Kévin <wolff.kevin@protonmail.com>
2024-06-21 18:14:43 +0200

    Migrate admin header component

--- frontend/src/ui/admin/molecules/header/Header.tsx
+++ frontend/src/ui/admin/molecules/header/Header.tsx
@@ -1,24 +1,23 @@
-"use client";
-
+import { HTMLAttributes } from "react";
 import Image from "next/image";
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import classNames from "classnames";
+import { Link } from "@/ui/admin/atoms/link/Link";
 import "./header.scss";
 
-import { Link } from "@/ui/admin/atoms/link/Link";
-
-// TODO: update to server side.
-export function Header() {
-  const { data: session, status } = useSession();
+export async function Header(props: HTMLAttributes<HTMLElement>) {
+  const session = await getServerSession();
+  const headerClasses = classNames("admin__header", props.className);
 
   return (
-    <header className="admin__header">
+    <header className={headerClasses} {...props}>
       <div className="admin__header__nav">
         <div className="admin__header__nav__brand">
           <div className="admin__header__nav__brand__logo">
             <Link href="/">Perfect Page</Link>
           </div>
         </div>
-        {status === "authenticated" && session && (
+        {session && session.user && session.user.name && (
           <div className="admin__header__nav__user">
             <p>{session?.user.name}</p>
             {session.user.image && (
